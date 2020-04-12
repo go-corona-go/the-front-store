da790310fafee653a843820cc018652024e6de27
Prateek Gogia <gogia.prateek@hotmail.com>
2020-04-12 14:56:27 +0530

    Product Card data is working now along with links

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -4,6 +4,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Link from 'next/link';
 
 const useStyles = makeStyles({
   root: {
@@ -11,29 +12,32 @@ const useStyles = makeStyles({
   },
 });
 
-export default function ProductCard() {
+export default function ProductCard({ product }) {
   const classes = useStyles();
 
+  const { id, name, image_link } = product;
+
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="https://images-na.ssl-images-amazon.com/images/I/71YPSip7ELL._SX679_.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Capitol Venus N95 Particulate Respirator Dust Masks Disposable Anti
-            Pollution Mask with Exhalation Valve
-          </Typography>
-          <Typography gutterBottom component="p">
-            US $0.01 - 3.32
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Link href={`/desc/${id}`}>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt={name}
+            height="140"
+            image={image_link}
+            title={name}
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {name}
+            </Typography>
+            <Typography gutterBottom component="p">
+              US $0.01 - 3.32
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 }
--- pages/homepage.js
+++ pages/homepage.js
@@ -2,11 +2,31 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import { useState } from 'react';
 import Navbar from '../components/Navbar';
-import ProductCard from '../components/product-card';
+import ProductCard from '../components/ProductCard';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_ALL_PRODUCTS = gql`
+  query getAllProducts {
+    inventory_items {
+      id
+      image_link
+      name
+      category
+      description
+    }
+  }
+`;
 
 const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
+    padding: '20px',
+  },
+  banner: {
+    backgroundColor: 'black',
+    height: '500px',
+    width: '100%',
+    borderRadius: '0 0 55% 55%',
   },
   paper: {
     height: 140,
@@ -21,41 +41,48 @@ const Homepage = () => {
   const [spacing, setSpacing] = useState(1);
   const classes = useStyles();
 
-  const handleChange = (event) => {
-    setSpacing(Number(event.target.value));
-  };
+  const { data, loading, error } = useQuery(GET_ALL_PRODUCTS);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error...</div>;
+  if (!data) return <div>No Products available</div>;
+  const { inventory_items } = data;
+
   return (
     <>
       <Navbar />
-      <section>
-        <h1>Our Mission</h1>
-        <p className={classes.mission}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-      </section>
-      <section>
-        <h1>Products</h1>
+      <div className={classes.banner}></div>
+      <div className={classes.root}>
+        <section>
+          <h1>Our Mission</h1>
+          <p className={classes.mission}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </section>
+        <section>
+          <h1>Products</h1>
 
-        <Grid container className={classes.root} spacing={2}>
-          <Grid item xs={12}>
-            <Grid container justify="flex-start" spacing={spacing}>
-              {[0, 1, 2, 3, 4].map((value) => (
-                <Grid key={value} item>
-                  <ProductCard />
-                </Grid>
-              ))}
+          <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+              <Grid container justify="flex-start" spacing={spacing}>
+                {inventory_items.map((product) => (
+                  <Grid key={product.id} item>
+                    <ProductCard product={product} />
+                  </Grid>
+                ))}
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </section>
+        </section>
+      </div>
     </>
   );
 };
