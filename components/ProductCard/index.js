ed3c699e284b714b5f9e98fe26524f79f004bc09
vilvaathibanpb <vilvaathiban@gmail.com>
2020-04-12 13:30:29 +0200

    fix: css issue

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable babel/camelcase */
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -5,10 +6,58 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Link from 'next/link';
+import { GridListTile } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 245,
+  },
+  tile: {
+    borderRadius: '20px',
+    padding: '20px!important',
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexDirection: 'column',
+    cursor: 'pointer',
+  },
+  img: {
+    maxHeight: '214px',
+    maxWidth: '214px',
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  imageContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '214px',
+    maxHeight: '214px',
+    width: '100%',
+    justifyContent: 'center',
+  },
+  productName: {
+    fontSize: '14px',
+    lineHeight: '19px',
+    color: '#000000',
+    marginBottom: '12px',
+    marginTop: '12px',
+  },
+  priceRange: {
+    fontWeight: '600',
+    fontSize: '14px',
+    lineHeight: '19px',
+    color: '#000000',
+  },
+  container: {
+    background: '#fff',
+    maxWidth: '100%',
+    maxWeight: '100%',
+    padding: '20px',
+    borderRadius: '20px',
+    boxSizing: 'border-box',
+    border: '1px solid rgba(0,0,0,0.1)',
   },
 });
 
@@ -18,26 +67,21 @@ export default function ProductCard({ product }) {
   const { id, name, image_link } = product;
 
   return (
-    <Link href={`/desc/${id}`}>
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt={name}
-            height="140"
-            image={image_link}
-            title={name}
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {name}
-            </Typography>
-            <Typography gutterBottom component="p">
-              US $0.01 - 3.32
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+    <Link href="/desc/[id]" as={`/desc/${product.id}`} key={product.id}>
+      <GridListTile className={classes.tile} cols={1}>
+        <div className={classes.container}>
+          <div className={classes.imageContainer}>
+            <img
+              src={product.image_link}
+              alt={product.name}
+              className={classes.img}
+            />
+          </div>
+          <div className={classes.productName}>{product.name}</div>
+          <div className={classes.priceRange}>{product.min_price} - 
+    ${product.max_price}</div>
+        </div>
+      </GridListTile>
     </Link>
   );
 }
